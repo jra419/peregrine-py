2b6e2a919f6afa469ee17ea891e57caa979b0bcb
Francisco Pereira <fchamicapereira@gmail.com>
2023-02-13 19:45:49 +0000

    now measuring bps

--- testbed/bench-kitsune.py
+++ testbed/bench-kitsune.py
@@ -40,10 +40,17 @@ def run_benchmarks(attacks, pcaps_dir):
 		with open(csv, 'r') as f:
 			lines = f.readlines()
 			assert len(lines) >= 1
+			
 			line = lines[0].rstrip('\n')
-			training_time, execution_time, pps = line.split(',')
-			results.append((name, training_time, execution_time, pps))
-			print(f"      rate={util.compact(int(pps))}pps")
+			line = line.split(',')
+
+			training_time  = line[0]
+			execution_time = line[1]
+			pps            = int(line[2])
+			bps            = int(line[3])
+
+			results.append((name, training_time, execution_time, pps, bps))
+			print(f"      rate {util.compact(int(pps))}pps {util.compact(int(bps))}bps")
 
 		os.remove(csv)
 	
@@ -51,6 +58,7 @@ def run_benchmarks(attacks, pcaps_dir):
 
 def dump_stats():
 	ppss = []
+	bpss = []
 
 	with open(TEST_RESULTS, 'r') as f:
 		lines = f.readlines()
@@ -59,27 +67,42 @@ def dump_stats():
 			line = line.split(',')
 
 			if not line: continue
-			assert len(line) == 4
+			assert len(line) == 5
 
-			name = line[0]
-			training_time = line[1]
+			name           = line[0]
+			training_time  = line[1]
 			execution_time = line[2]
-			pps = int(line[3])
+			pps            = int(line[3])
+			bps            = int(line[4])
+
 			ppss.append(pps)
+			bpss.append(bps)
 
 	with open(TEST_STATS, 'w') as f:
 		if len(ppss) == 1:
-			m     = ppss[0]
-			mean  = ppss[0]
-			stdev = 0
-			M     = ppss[0]
-		else:
-			m     = min(ppss)
-			mean  = statistics.mean(ppss)
-			stdev = statistics.stdev(ppss)
-			M     = max(ppss)
+			pps_m     = ppss[0]
+			pps_mean  = int(ppss[0])
+			pps_stdev = int(0)
+			pps_M     = ppss[0]
 
-		f.write(f"{m},{mean},{stdev},{M}\n")	
+			bps_m     = bpss[0]
+			bps_mean  = int(bpss[0])
+			bps_stdev = int(0)
+			bps_M     = bpss[0]
+		else:
+			pps_m     = min(ppss)
+			pps_mean  = int(statistics.mean(ppss))
+			pps_stdev = int(statistics.stdev(ppss))
+			pps_M     = max(ppss)
+
+			bps_m     = min(bpss)
+			bps_mean  = int(statistics.mean(bpss))
+			bps_stdev = int(statistics.stdev(bpss))
+			bps_M     = max(bpss)
+
+		f.write(f"{pps_m},{pps_mean},{pps_stdev},{pps_M},")
+		f.write(f"{bps_m},{bps_mean},{bps_stdev},{bps_M}")
+		f.write(f"\n")
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Benchmark Kitsune')
@@ -105,7 +128,7 @@ if __name__ == '__main__':
 		results = run_benchmarks(target_tests, args.pcaps_dir)
 
 		with open(TEST_RESULTS, 'w') as f:
-			for attack, training_time, execution_time, pps in results:
-				f.write(f'{attack},{training_time},{execution_time},{pps}\n')
+			for attack, training_time, execution_time, pps, bps in results:
+				f.write(f'{attack},{training_time},{execution_time},{pps},{bps}\n')
 	
 	dump_stats()
---
+++ testbed/plot-component-bench.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import util
+import hosts
+import os
+import argparse
+import glob
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+SCRIPT_DIR       = os.path.dirname(os.path.realpath(__file__))
+TEST_RESULTS_DIR = f'{SCRIPT_DIR}/results'
+PLOT             = f'{TEST_RESULTS_DIR}/component-bench.png'
+KITSUNE_DATA     = f'{TEST_RESULTS_DIR}/kitsune/stats.csv'
+
+def get_kitsune_data():
+	with open(KITSUNE_DATA, 'r') as f:
+		lines = f.readlines()
+		assert len(lines) == 1
+
+		line = lines[0]
+		line = line.rstrip('\n')
+		line = line.split(',')
+
+		assert len(line) == 4
+
+		m     = int(line[0])
+		avg   = int(line[1])
+		stdev = int(line[2])
+		M     = int(line[3])
+		
+		return avg, stdev
+
+def gen_plot(data, plot_file, pps=False, bps=False):
+	assert pps or bps
+
+	fig = plt.figure()
+	plt.clf()
+
+	ax = fig.add_subplot(111)
+	ax.set_aspect(1)
+
+	min_sampling_rate, max_sampling_rate = get_sampling_rate_range(data)
+
+	if bps:
+		vmin = 0
+		vmax = int(100e9)
+		units = 'bps'
+	else:
+		vmin = 0
+		vmax = int(50e6)
+		units = 'pps'
+	
+	attacks        = sorted(list(data.keys()))
+	sampling_rates = []
+	matrix         = [ [] for _ in range(len(attacks)) ]
+
+	for i, attack in enumerate(attacks):
+		attack_vector = data[attack]
+		attack_vector.sort(key=lambda v: v[0])
+
+		for sampling_rate,tg_rate,rx_rate_bps,rx_rate_pps,tx_rate_pps in attack_vector:
+			if pps: matrix[i].append(rx_rate_pps)
+			else:   matrix[i].append(rx_rate_bps)
+
+			sampling_rates = list(set(sampling_rates + [ sampling_rate ]))
+
+	# print('Sampling rates', sampling_rates)
+	# print('Attacks', attacks)
+
+	# colormap = plt.cm.jet
+	colormap = mpl.colormaps['RdYlGn']
+	
+	nodes  = [ 0.0, 0.5, 1.0 ]
+	colors = [ 'crimson', 'yellow', 'mediumseagreen' ]
+	cmap   = mpl.colors.LinearSegmentedColormap.from_list("mycmap", list(zip(nodes, colors)))
+	cmap.set_under('black')
+
+	res = ax.imshow(matrix, cmap=cmap, vmin=vmin, vmax=vmax, interpolation='nearest', aspect='auto')
+
+	for x in range(len(matrix)):
+		for y in range(len(matrix[x])):
+			v = matrix[x][y]
+			ax.annotate(f'{util.compact(v, no_decimal=True)}{units}', xy=(y,x), va='center', ha='center')
+
+	n_ticks = 6
+	ticks = []
+	labels = []
+	for i in range(n_ticks):
+		tick = int(vmax*i/(n_ticks-1))
+		ticks.append(tick)
+		labels.append(f'{util.compact(tick, no_decimal=True)}{units}')
+
+	cbar = fig.colorbar(res, ticks=ticks)
+	cbar.ax.set_yticklabels(labels)
+
+	width  = len(sampling_rates)
+	height = len(attacks)
+
+	plt.xticks(range(width), sampling_rates)
+	plt.yticks(range(height), attacks)
+
+	plt.savefig(plot_file, bbox_inches='tight')
+	# plt.show()
+
+def plot():
+	peregrine_dataplane_data = ()
+	kitsune_data = get_kitsune_data()
+	print(kitsune_data)
+
+	# if not data:
+	# 	print('No data.')
+	# 	exit(0)
+
+	# print('Generating pps plot')
+	# gen_plot(data, PLOT_PPS, pps=True)
+
+	# print('Generating bps plot')
+	# gen_plot(data, PLOT_BPS, bps=True)
+
+	print('Done')
+
+if __name__ == '__main__':
+	plot()
